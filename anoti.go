813b103a5cf6e2c954e86f8b91a1d2edf93a47ee
SeongJae Park <sj38.park@gmail.com>
2016-03-01 13:53:36 +0900

    introduce task concept and its file format
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- anoti.go
+++ anoti.go
@@ -1,12 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/smtp"
 	"os"
 	"strconv"
 )
+
+type task struct {
+	Cmd         string
+	FailMsg     string
+	Receipients []string
+}
+
+var tasks []task
+
+func read_tasks(filePath string) {
+	c, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Printf("failed to read task file %s: %s\n", filePath, err)
+		return
+	}
+
+	if err := json.Unmarshal(c, &tasks); err != nil {
+		log.Printf("failed to unmarshal task file %s: %s\n",
+			filePath, err)
+		return
+	}
+
+	for i, t := range tasks {
+		fmt.Printf("task %d\n", i)
+		fmt.Printf("cmd: %s\nFail msg: %s\nReceipients:%s\n\n",
+			t.Cmd, t.FailMsg, t.Receipients)
+	}
+}
+
+func write_sample_tasks(filePath string) {
+	tasks = []task{task{Cmd: "abc_cmd", FailMsg: "fail_msg",
+		Receipients: []string{"receipients 1", "1"}}}
+	bytes, err := json.Marshal(tasks)
+	if err != nil {
+		log.Printf("failed to marshal tasks: %s\n", err)
+		return
+	}
+
+	if err := ioutil.WriteFile(filePath, bytes, 0600); err != nil {
+		log.Printf("failed to write marshaled tasks: %s\n", err)
+		return
+	}
+}
 
 func send_mail(username, password, hostname string, port int,
 	sender string, receipients []string,
@@ -32,6 +77,11 @@ func main() {
 	for i, a := range args {
 		fmt.Println(i, " ", a)
 	}
+	if len(args) == 2 {
+		read_tasks(args[1])
+		return
+	}
+
 	port, e := strconv.Atoi(args[4])
 	if e != nil {
 		log.Fatal("wrong argument to port number!", e)
